993150a84b658cfff297569d07cb31b5333a6838
Shubham Pandey <sp9047209@gmail.com>
2024-11-16 19:57:02 +0530

    sloved problem 2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        problem1(5);
+        problem2(5);
+    }
+
+    public static void problem2(int rows) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     public static void problem1(int rows) {
